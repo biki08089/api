9cd14566b1b3f140adfae2649feec05b97ff241c
biki08089 <bikashpradhan99048@gmail.com>
2024-03-19 02:47:00 +0530

    nbvngbv

--- myfirstApi/index.js
+++ myfirstApi/index.js
@@ -3,19 +3,9 @@ const app = express();
 require("dotenv").config();
 const dbConnect = require("./config/dataBase");
 const PORT = process.env.PORT || 2089;
-<<<<<<< HEAD
 const cors=require("cors");
 app.use(express.json());
 app.use(cors());
-=======
-app.listen(PORT, () => {
-    console.log("We are running on " + PORT);
-})
-app.get("/", (req, res) => {
-    res.send("Hello world");
-})
-dbConnect();
->>>>>>> 75171ea5ce0093fb8d4185572f675e726c2fdd96
 
 console.log("fhdffddhdljhgkdfhgjfdkjghfdhghggdfghfghhghfghghfdhg")
 
